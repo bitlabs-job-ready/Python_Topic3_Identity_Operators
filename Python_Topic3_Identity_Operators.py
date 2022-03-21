421254e63e5500629ef5a9aee0c3ebbdac1a07eb
bitLabs <75418136+bitlabs-job-ready@users.noreply.github.com>
2022-03-21 12:35:50 +0530

    Add files via upload

---
+++ Python_Topic3_Identity_Operators.py
@@ -0,0 +1,26 @@
+# Identity Operators
+# In Place of Question Mark you need to fill with Identity operator
+
+a = 20
+b = 20
+
+if ( ? ):
+   print ("Line 1 - a and b have same identity")
+else:
+   print("Line 1 - a and b do not have same identity")
+
+if ( ? ):
+   print("Line 2 - a and b have same identity")
+else:
+   print("Line 2 - a and b do not have same identity")
+
+b = 30
+if ( ? ):
+   print("Line 3 - a and b have same identity")
+else:
+   print("Line 3 - a and b do not have same identity")
+
+if ( ? ):
+   print("Line 4 - a and b do not have same identity")
+else:
+   print("Line 4 - a and b have same identity")
